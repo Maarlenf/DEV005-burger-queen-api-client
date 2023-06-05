a310c4f4f99d83f2029c922417cf22f3a0822220
ifdotcom <ifervb@gmail.com>
2023-06-05 18:11:38 -0500

    add desing for first view

--- burger-queen-fer-marlen/src/components/home/App.jsx
+++ burger-queen-fer-marlen/src/components/home/App.jsx
@@ -1,37 +1,61 @@
-import { useState } from 'react'
+// import { useState } from "react";
 // import reactLogo from '../../assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import  bannerBurguer from '../../assets/bannerHamburguesa.png'
+import "./App.css";
+import bannerBurguer from "../../assets/bannerHamburguesa.png";
+import logo from "../../assets/logo.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <img src={bannerBurguer} className='banner' alt='Banner Burguer Queen' />
+        <img
+          src={bannerBurguer}
+          className='banner'
+          alt='Banner Burguer Queen'
+        />
         {/* <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> */}
-          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
+        {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
         {/* </a> */}
         {/* <a href="https://react.dev" target="_blank" rel="noreferrer"> */}
-          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
+        {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
         {/* </a> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <img src={logo} className='logo' alt='Logo Burguer Queen' />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Iniciar Sesión</h1>
+      <div className='formLogin'>
+        <form method='post'>
+          <label>
+            <p>Correo Electrónico</p>
+            <input
+              className='input'
+              id='email'
+              name='myInput'
+              placeholder='example@example'
+            />
+          </label>
+          <label>
+            <p>Contraseña</p>
+            <input
+              className='input'
+              id='password'
+              name='myInput'
+              placeholder='******'
+            />
+          </label>
+
+          <button id='btnLogin' type='submit'>
+            Iniciar sesión
+          </button>
+        </form>
+      </div>
+      <footer>
+        <p>Creado por @marlen & @fer</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
