349b333dd84e9243e599d18b581d4c104f3903ed
Maarlenf <marlenefigueroa.a22@gmail.com>
2023-06-06 19:55:01 -0400

    add new function app, first page

--- burger-queen-fer-marlen/src/components/home/App.jsx
+++ burger-queen-fer-marlen/src/components/home/App.jsx
@@ -2,36 +2,82 @@ import { useState } from 'react'
 // import reactLogo from '../../assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import  bannerBurguer from '../../assets/bannerHamburguesa.png'
+import  bannerBurger from '../../assets/bannerBurgerOpaca.png'
+import logoBurger from '../../assets/logoBurguerQueen.png'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [valueEmail, setEmail] = useState("");
+ const [valuePwd, setPwd] = useState("");
+ const [fail, setError] =useState("");
+ const login = async () => {
+  try {
+    const respuesta = await fetch("http://localhost:8080/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: valueEmail,
+        password: valuePwd,
+      }),
+    });
 
+    if (respuesta.ok) {
+      // La petición fue exitosa
+      const datos = await respuesta.json();
+      console.log(datos);
+    } else {
+      // La petición falló
+      // eslint-disable-next-line no-unused-vars
+      const error = await respuesta.json();
+      throw new Error(setError('¡Ups! Algo salío mal. Compruebe sus credenciales'));
+    }
+  } catch (error) {
+    // Manejo de errores, por ejemplo, mostrar un mensaje de error en la interfaz.
+    console.error(error);
+  }
+};
+function handleSubmit(e) {
+// Previene que el navegador recargue la página
+e.preventDefault();
+// Lee los datos del formulario
+login();
+
+}
   return (
     <>
       <div>
-        <img src={bannerBurguer} className='banner' alt='Banner Burguer Queen' />
-        {/* <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> */}
-          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
-        {/* </a> */}
-        {/* <a href="https://react.dev" target="_blank" rel="noreferrer"> */}
-          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
-        {/* </a> */}
+        <img src={bannerBurger} className='banner' alt='Banner Burger Queen' />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <img src={logoBurger} className = 'logo' alt='Logo Burger Queen' />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Iniciar sesión</h1>
+      <div className="formLogin">
+       <form method="post" onSubmit={handleSubmit}>
+        <label>
+          <p className='p-login'>Correo electrónico</p>
+          <input type='email' id='email' className= 'input' placeholder='example@live.cl' value={valueEmail} 
+          // ... fuerza al valor del input para que coincida con la variable de estado...
+      onChange={e => setEmail(e.target.value)}/>
+        </label>
+        <label>
+          <p className='p-login'>Contraseña</p>
+          <input type='password' id='password' className='input' placeholder='*******' autoComplete='current-password' value={valuePwd} 
+          // ... fuerza al valor del input para que coincida con la variable de estado...
+      onChange={e => setPwd(e.target.value)}/>
+          <span className='failLogin'>{fail}</span>
+        </label>
+        <label>
+          <button type= 'submit' id='btnLogin' className='btn'>Iniciar sesión</button>
+        </label>
+       </form>
+      </div>
+      <footer><p>Created by @Fer and @Marlen</p></footer>
     </>
   )
 }
 
 export default App
+
--- burger-queen-fer-marlen/src/main.jsx
+++ burger-queen-fer-marlen/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/home/App'
+// import Mentira from '.components/home/Mentira'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App />,
+    {/* <Mentira /> */}
   </React.StrictMode>,
 )
