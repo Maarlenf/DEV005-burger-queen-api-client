0860ce0ee0c5f89ed341f143759961513ea8ab74
Maarlenf <marlenefigueroa.a22@gmail.com>
2023-07-10 19:47:16 -0400

    change title and icon

--- burger-queen-fer-marlen/src/images.js
+++ burger-queen-fer-marlen/src/images.js
@@ -1,7 +1,8 @@
 import bannerBurger from './assets/banner-opacity.png';
 import logo from './assets/logo.png';
-
+import icon from './assets/imageIcon.png';
 export {
     bannerBurger,
-    logo
+    logo,
+    icon
 }
